544c6d6668c1b66f9bebe79c464bd77451a6b8c5
ChanwooPark <chanw9503@gmail.com>
2023-03-24 13:49:07 +0900

    [START] first commit

---
+++ src/axios/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+
+  headers: {
+    Authorization: '',
+  },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return error;
+  }
+);
+
+instance.interceptors.response.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return error;
+  }
+);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/configs/configStore';
+import { QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <QueryClientProvider>
+      <App />
+    </QueryClientProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
