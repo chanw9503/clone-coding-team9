e910929d0af733426847fc5a47febff46738a373
yuny0y <yuny0y@naver.com>
2023-03-25 14:57:07 +0900

    [FEAT] :  ⚙️ SignUp add

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -5,52 +5,184 @@
 */
 
 import React from 'react';
-import styled from 'styled-components';
+// import { emailCheck, nickNameCheck, pwCheck, reconfirmPw } from '../../utils/regExp';
+import FormInput from '../../components/Input/FormInput'
+import { StContainer, StForm, StSns, StTitle } from './Styles';
+import { useMutation, useQueryClient } from 'react-query';
+import { addSign } from '../../api/Signup';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 function SignUp() {
-   
-  return (
-    <StContainer>
+    const navigate = useNavigate();
+    const [nickName, setNickName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [selected, setSelected] = useState('');
+
+    //오류메시지 상태저장
+    const [NickNameMessage, setNickNameMessage] = useState('');
+    const [emailMessage, setEmailMessage] = useState('');
+    const [passwordMessage, setPasswordMessage] = useState('');
+    const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
+
+    // 유효성 검사
+    const [isNickName, setIsNickName] = useState(false);
+    const [isEmail, setIsEmail] = useState(false);
+    const [isPassword, setIsPassword] = useState(false);
+    const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+    
+    const queryClient = useQueryClient();
+    const mutation =useMutation(addSign, {
+        onSuccess : () => {
+            queryClient.invalidateQueries("Signup")
+        }
+    });
+
+    const newUser = {
+        id: 2,
+        userEmail: email +"@" + selected,
+        nickname: nickName,
+        password: password,
+        confirm: passwordConfirm,
+      };
+
+    const onSubmit =  (e) => {
+        e.preventDefault()
+        mutation.mutate(newUser)
+        // navigate('/');
+    };
+    
+    // 이메일
+    const onChangeEmail = useCallback((e) => {
+        const emailRegex =
+        /^(?=.*[a-zA-Z])(?=.[0-9])$/
+        const emailCurrent = e.target.value
+        setEmail(emailCurrent)
+    
+        if (!emailRegex.test(emailCurrent)) {
+        setEmailMessage('이메일 형식이 올바르지 않습니다.')
+        setIsEmail(false)
+        } else {
+        setEmailMessage('');
+        setIsEmail(true)
+        }
+    }, [])
+
+    // 비밀번호
+    const onChangePassword = useCallback((e) => {
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*()+=-])(?=.*[0-9]).{8,15}$/;
+        const passwordCurrent = e.target.value
+        setPassword(passwordCurrent)
+    
+        if (!passwordRegex.test(passwordCurrent)) {
+        setPasswordMessage('비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다');
+        setIsPassword(false);
+        } else {
+        setPasswordMessage('');
+        setIsPassword(true);
+        }
+    }, [])
+
+    // 비밀번호 확인
+    const onChangePasswordConfirm = useCallback(
+        (e) => {
+        const passwordConfirmCurrent = e.target.value
+        setPasswordConfirm(passwordConfirmCurrent)
+    
+        if (password === passwordConfirmCurrent) {
+            setPasswordConfirmMessage('');
+            setIsPasswordConfirm(true);
+        } else {
+            setPasswordConfirmMessage('비밀번호가 일치하지 않습니다.');
+            setIsPasswordConfirm(false);
+        }
+        },
+        [password]
+    )
+
+    // 이름
+    const onChangeNickName = useCallback((e) => {
+        setNickName(e.target.value)
+        if (e.target.value.length < 2 ||e.target.value.length > 30 ) {
+            setNickNameMessage('2글자 이상 입력해주세요.')
+            setIsNickName(false)
+        } else {
+            setNickNameMessage('올바른 이름 형식입니다 :)')
+            setIsNickName(true)
+        }
+    }, [])
+
+
+        
+    return (
+        <StContainer>
             <StForm>
             <StTitle>회원가입</StTitle>
             <StSns>
                 <div>SNS계정으로 간편하게 회원가입</div>
             </StSns>
             <div>이메일</div>
-            <input />@<input />
-            <div>비밀번호</div>
-            <input />
-            <div>비밀번호확인</div>
-            <input />
-            <div>닉네임</div>
-            <input />
+            <div>
+                <FormInput 
+                type="text" 
+                size="small" 
+                value={email} 
+                onChange={onChangeEmail}/>
+                <span>@</span>
+                <select value={selected} onChange={(e)=> {
+                    setSelected(e.target.value)
+                }}>
+                    <option value disabled>선택해주세요</option>
+                    <option value="naver.com">naver.com</option>
+                    <option value="hanmail.net">hanmail.net</option>
+                    <option value="daum.net">daum.net</option>
+                    <option value="gamail.com">gamail.com</option>
+                    <option value="nate.com">nate.com</option>
+                    <option value="hotmail.com">hotmail.com</option>
+                    <option value="outlook.com">outlook.com</option>
+                    <option value="icloud.com">icloud.com</option>
+                </select>
+                {email.length > 0 && 
+                <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
+            </div>
+            <button>이메일 인증하기</button>
+            <FormInput
+                type = "password"
+                size="medium"
+                label="비밀번호"
+                placeholder="비밀번호"
+                value ={password}
+                onChange={onChangePassword}
+            />
+            {password.length > 0 && (
+            <span className={`message ${isPassword ? 'success' : 'error'}`}>{passwordMessage}</span>
+            )}
+            <FormInput
+                type = "password"
+                size="medium"
+                label="비밀번호 확인"
+                placeholder="비밀번호 확인"
+                value={passwordConfirm}
+                onChange={onChangePasswordConfirm}
+            />
+            <span className={`message ${isPasswordConfirm ? 'success' : 'error'}`}>{passwordConfirmMessage}</span>
+            <FormInput
+                size="medium"
+                label="닉네임"
+                labelcomment="다른 유저와 겹치지 않도록 입력해주세요. (2~15자)"
+                placeholder="별명 (2~15자)"
+                value={nickName}
+                onChange={onChangeNickName}
+            />
+            {nickName.length > 0 && <span className={`message ${isNickName ? 'success' : 'error'}`}>{NickNameMessage}</span>}
+            <button type="submit" onClick={onSubmit}>회원가입하기</button>
+            <div>이미 아이디가 있으신가요? </div>
             </StForm>
-    </StContainer>
-  );
+        </StContainer>
+    );
 }
 
-export default SignUp
-
-const StContainer = styled.div`
-    padding-top: 40px;
-    width: calc(100% - 30px);
-    margin: auto;
-    display: block;
-`
-const StTitle = styled.h1`
-    font-size: 20px;
-    font-weight: bold;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-`
-const StSns = styled.div`
-    margin: 30px 0;
-    padding-bottom: 30px;
-    border-bottom: 1px solid #ededed;
-`
-
-const StForm = styled.div`
-    width: 360px;
-    margin : 0 auto;
-    padding: 60px 0;
-`
+export default SignUp;
