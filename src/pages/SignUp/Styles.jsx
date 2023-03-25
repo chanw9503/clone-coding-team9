faa2bab05e9d2ea07b555a49698bfb03cdf05fdf
yuny0y <yuny0y@naver.com>
2023-03-25 14:57:52 +0900

    [STYLE] :  🎨 SignUp Styles add

--- src/pages/SignUp/Styles.jsx
+++ src/pages/SignUp/Styles.jsx
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const StContainer = styled.div`
+    padding-top: 40px;
+    width: calc(100% - 30px);
+    margin: auto;
+    display: block;
+`
+export const StTitle = styled.h1`
+    font-size: 20px;
+    font-weight: bold;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+`
+export const StSns = styled.div`
+    margin: 30px 0;
+    padding-bottom: 30px;
+    border-bottom: 1px solid #ededed;
+`
+
+export const StForm = styled.form`
+    width: 360px;
+    margin : 0 auto;
+    padding: 60px 0;
+`
