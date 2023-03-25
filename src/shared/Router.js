3ae8e4ea45a12dca84aa11dd46941a29d190a4b9
ChanwooPark <chanw9503@gmail.com>
2023-03-25 14:58:25 +0900

    [TEST] : 📝 테스트 Router 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import Login from '../pages/Login';
 import Main from '../pages/Main/Main';
 import SignUp from '../pages/SignUp/SignUp';
@@ -11,7 +12,8 @@ function Router() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/header" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </div>
