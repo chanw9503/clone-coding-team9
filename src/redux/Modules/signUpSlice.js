431164203a590ce0382d7f9040f4a28e738693a1
yuny0y <yuny0y@naver.com>
2023-03-25 15:00:56 +0900

    [DOCS] : 📚 signUpSlice Modify

--- src/redux/Modules/signUpSlice.js
+++ src/redux/Modules/signUpSlice.js
@@ -1,42 +1,16 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { instance } from "../../axios/api";
-
-export const __signUpUser = createAsyncThunk(
-    "signUpUsers",
-    async (payload, thunkAPI) => {
-        try {
-            await instance.post("/signup", payload);
-            return thunkAPI.fulfillWithValue(payload);
-        } catch(error) {
-            return thunkAPI.rejectWithValue(error);
-        }
-    }
-);
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    users: [],
-    error: null,
-    isLogin: false,
+    email: "",
+    nickName : "",
+    password : "",
+    confirm : "",
 };
 
 export const signUpSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {},
-    extraReducers: {
-        //signupUser
-        [__signUpUser.pending]: (state) => {
-            state.isLogin = true;
-        },
-        [__signUpUser.fulfilled]: (state, action) => {
-            state.isLogin = [...state.users, action.payload]
-            state.users = action.payload;
-        },
-        [__signUpUser.rejected]: (state, action) => {
-            state.isLogin = false;
-            state.error = action.payload;
-        },
-    }
-})
+});
 
 export default signUpSlice.reducer
