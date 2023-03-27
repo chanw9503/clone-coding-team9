d9c4c644c1a29c1bc82725b376b610392346ec98
ChanwooPark <chanw9503@gmail.com>
2023-03-27 15:23:44 +0900

    [FEAT] : ⚙️ 로그인 /회원가입 경로 설졍

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import LogoType from '../LogoType/LogoType';
+import Navigation from '../Navigation';
 import {
   StyledHeader,
   StyledContainer,
@@ -11,6 +13,7 @@ import {
 } from './styles';
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <StyledHeader>
       <StyledContainer>
@@ -20,22 +23,20 @@ function Header() {
               <LogoType />
             </StyledFlexBlock>
             <StyledFlexBlock>
-              <StyledLoginBlock>로그인</StyledLoginBlock>
-              <StyledLoginBlock>회원가입</StyledLoginBlock>
+              <StyledLoginBlock onClick={() => navigate('/login')} borderLight={true}>
+                로그인
+              </StyledLoginBlock>
+              <StyledLoginBlock onClick={() => navigate('/signup')}>
+                회원가입
+              </StyledLoginBlock>
               <StyledWritingButton>글쓰기</StyledWritingButton>
             </StyledFlexBlock>
           </StyledItemBlock>
         </StyledBoardBlock>
       </StyledContainer>
+      <Navigation />
     </StyledHeader>
   );
 }
 
 export default Header;
-
-/**
- * flex: 0 0 auto;
- * 1 item : flex 컨테이너 내에서 늘어나는(flex-grow) 비율  "0" 은 늘어나지 않는것을 의밎
- * 2 item : flex 컨테이너 내에서 축소(shirk)되는 비율을 나타나며, "0"은 축소되지 않는 것을 의미
- * 3 item : 아이템의 초기 크기, "auto" 는 컨테이너의 가능한 최대 크기까지 아이템이 늘어날 수 있다는 것을 의미
- */
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,15 @@
 import styled, { css } from 'styled-components';
-import { COLOR } from '../../utils/color';
+import { COLOR } from '../../shared/color';
+
+/**
+ * flex: 0 0 auto;
+ * 1 item : flex 컨테이너 내에서 늘어나는(flex-grow) 비율  "0" 은 늘어나지 않는것을 의밎
+ * 2 item : flex 컨테이너 내에서 축소(shirk)되는 비율을 나타나며, "0"은 축소되지 않는 것을 의미
+ * 3 item : 아이템의 초기 크기, "auto" 는 컨테이너의 가능한 최대 크기까지 아이템이 늘어날 수 있다는 것을 의미
+ */
+
 const StyledHeader = styled.header`
-  flex: 0 0 auto; // 1 item : flex 컨테이너 내에서 늘어나는
+  flex: 0 0 auto;
   box-sizing: border-box;
   position: relative;
   display: block;
@@ -16,7 +24,7 @@ const StyledBoardBlock = styled.div`
   z-index: 1102;
   top: 0px;
   width: 100%;
-
+  background-color: ${COLOR.white};
   display: flex;
   justify-content: center;
   align-items: center;
