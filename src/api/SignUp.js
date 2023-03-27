49734d2f528e95370ab83dc4e3b5f073204ffa8a
yuny0y <yuny0y@naver.com>
2023-03-27 15:12:21 +0900

    [DOCS] : 📚 Email cofirm post add

--- src/api/SignUp.js
+++ src/api/SignUp.js
@@ -3,4 +3,8 @@ import api from "../axios/api"
 const addSign = async(newUser) => {
     await api.post('/signup', newUser);
 }
-export {addSign};
+
+const confirmEmail = async(userEmail) => {
+    await api.post('/validate', userEmail)
+}
+export {addSign, confirmEmail};
