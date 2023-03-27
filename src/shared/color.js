d30133fa0a25ee8c8fa045f0aec1c58a12a9c66e
ChanwooPark <chanw9503@gmail.com>
2023-03-27 15:20:38 +0900

    [Rename] : 파일 위치 변경

---
+++ src/shared/color.js
@@ -0,0 +1,18 @@
+const COLOR = {
+  black: '#000000',
+  white: '#FFFFFF',
+  blue: '#35c5f0',
+  lightBlue: '#e3f8fa',
+  darkGray: '#757575',
+  lightGray: '#f5f5f5;',
+  buttonBlue: '#35c5f0',
+  boardRed: '#f77',
+  hoverRed: 'rgba(253, 170, 170, 0.3)',
+  hoverBlue: 'rgba(171, 201, 225, 0.3)',
+  hoverDeepBlue: '#1292b8',
+  borderGray: '#EAEDEF',
+  headerLogin: '#2F3438',
+  dividerGray: '#e0e0e0',
+};
+
+export { COLOR };
