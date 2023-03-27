7f5312caa706878048c3eaea940d9976d84b3927
ChanwooPark <chanw9503@gmail.com>
2023-03-27 15:21:19 +0900

    [FEAT] :  ⚙️ React-query devtools add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './shared/Router';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App() {
   const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
