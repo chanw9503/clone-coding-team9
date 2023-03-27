30dacb293e5a393e01d42fa47b00bb2e4017a26a
ChanwooPark <chanw9503@gmail.com>
2023-03-27 15:27:56 +0900

    [Remove] : 삭제

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '../components/Header/Header';
 import Login from '../pages/Login';
 import Main from '../pages/Main/Main';
 import SignUp from '../pages/SignUp/SignUp';
@@ -13,7 +12,6 @@ function Router() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/header" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </div>
