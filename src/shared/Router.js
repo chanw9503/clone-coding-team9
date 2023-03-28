a0e185e185f7c8f73ac93a23b1ec883b1c6bab5d
yuny0y <yuny0y@naver.com>
2023-03-28 23:37:30 +0900

    [DOCS] : 📚 posts Route add

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Main from '../pages/Main/Main';
+import Posts from '../pages/Posts/Posts';
 import SignUp from '../pages/SignUp/SignUp';
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/posts" element={<Posts />} />
         </Routes>
       </BrowserRouter>
     </div>
