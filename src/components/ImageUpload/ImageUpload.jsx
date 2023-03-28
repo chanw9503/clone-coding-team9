40e9a28fb9f08697fbe55aa128b1b7e234b2399b
yuny0y <yuny0y@naver.com>
2023-03-28 23:40:27 +0900

    [FEAT] : ⚙️ img 업로드 컴포넌트 add

---
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { StSendImage, StText, StyledImage } from './Styles'
+import SendImageIcon from '../../pages/Posts/SendImageIcon';
+import { useEffect } from 'react';
+
+function ImageUpload(props) {
+    const saveImage = (file) => {
+        if (file) {
+          URL.revokeObjectURL(props.image.preview_URL);
+          const preview_URL = URL.createObjectURL(file);
+          props.setImage({
+            image_file: file,
+            preview_URL: preview_URL,
+          });
+        }
+    };
+    
+    const ChangeHandler = (e) => {
+        e.preventDefault();
+        const files = e.target.files;
+        saveImage(files[0]);
+    };
+
+    useEffect(() => {
+    if (props.defaultImage !== '') {
+        props.setImage({ ...props.image, preview_URL: props.defaultImage });
+    }
+    return () => {
+        URL.revokeObjectURL(props.image.preview_URL);
+    };
+    }, []);
+    
+  return (
+    <>
+      <StSendImage htmlFor="dragdrop">
+        <input
+          type="file"
+          id="dragdrop"
+          multiple
+          accept="image/*"
+          hidden
+          required
+          onChange={ChangeHandler}
+        />
+        {
+            props.image.preview_URL ?  <StyledImage src={props.image.preview_URL}/> : 
+            <div>
+            <SendImageIcon />
+              <StText>사진올리기</StText>
+              <StText style={{ fontSize: "12px" }}>(*최대 10장까지)</StText>
+            </div>
+        }
+        </StSendImage>
+    </>
+  );
+};
+
+export default ImageUpload;
