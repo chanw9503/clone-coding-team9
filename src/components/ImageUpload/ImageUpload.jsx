1f91a248ee38a7aa2a8d7874370fe80960c65d6f
ChanwooPark <chanw9503@gmail.com>
2023-03-29 09:08:56 +0900

    [REFACTOR]: 🏭 file upload modify

--- src/components/ImageUpload/ImageUpload.jsx
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import { StSendImage, StText, StyledImage } from './Styles'
+import React from 'react';
+import { StSendImage, StText, StyledImage } from './Styles';
 import SendImageIcon from '../../pages/Posts/SendImageIcon';
 import { useEffect } from 'react';
+import { uploadFile } from '../../utils/upload';
 
 function ImageUpload(props) {
-    const saveImage = (file) => {
-        if (file) {
-          URL.revokeObjectURL(props.image.preview_URL);
-          const preview_URL = URL.createObjectURL(file);
-          props.setImage({
-            image_file: file,
-            preview_URL: preview_URL,
-          });
-        }
-    };
-    
-    const ChangeHandler = (e) => {
-        e.preventDefault();
-        const files = e.target.files;
-        saveImage(files[0]);
-    };
+  const saveImage = (file) => {
+    if (file) {
+      URL.revokeObjectURL(props.image.preview_URL);
+      const preview_URL = URL.createObjectURL(file);
 
-    useEffect(() => {
+      props.setImage({
+        image_file: file,
+        preview_URL: preview_URL,
+      });
+
+      const s3Location = uploadFile(file);
+      /**
+       * 부모에서 내려주는 props 추가
+       * const newLocationList = locationList
+       * setLocationList(newLocationList)
+       */
+    }
+  };
+
+  const ChangeHandler = (e) => {
+    e.preventDefault();
+    const files = e.target.files;
+    saveImage(files[0]);
+  };
+
+  useEffect(() => {
     if (props.defaultImage !== '') {
-        props.setImage({ ...props.image, preview_URL: props.defaultImage });
+      props.setImage({ ...props.image, preview_URL: props.defaultImage });
     }
     return () => {
-        URL.revokeObjectURL(props.image.preview_URL);
+      URL.revokeObjectURL(props.image.preview_URL);
     };
-    }, []);
-    
+  }, []);
+
   return (
     <>
       <StSendImage htmlFor="dragdrop">
@@ -42,17 +51,18 @@ function ImageUpload(props) {
           required
           onChange={ChangeHandler}
         />
-        {
-            props.image.preview_URL ?  <StyledImage src={props.image.preview_URL}/> : 
-            <div>
+        {props.image.preview_URL ? (
+          <StyledImage src={props.image.preview_URL} />
+        ) : (
+          <div>
             <SendImageIcon />
-              <StText>사진올리기</StText>
-              <StText style={{ fontSize: "12px" }}>(*최대 10장까지)</StText>
-            </div>
-        }
-        </StSendImage>
+            <StText>사진올리기</StText>
+            <StText style={{ fontSize: '12px' }}>(*최대 10장까지)</StText>
+          </div>
+        )}
+      </StSendImage>
     </>
   );
-};
+}
 
 export default ImageUpload;
