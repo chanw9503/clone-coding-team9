7a62f41570b937244aed32decf0364099ffa5864
ChanwooPark <chanw9503@gmail.com>
2023-03-30 12:09:43 +0900

    [RETACTOR] : 🏭 Options 파일 분리

---
+++ src/shared/options.js
@@ -0,0 +1,25 @@
+const sizeOptions = ['10평 미만', '10평대', '20평대', '30평대', '40평대', '50평대 이상'];
+const stlyeOptions = ['원룸&오피스텔', '아파트', '빌라&연립', '단독주택'];
+const lifeTypeOptions = [
+  '모던',
+  '북유럽',
+  '빈티지',
+  '내추럴',
+  '프로방스&로맨틱',
+  '한국&아시아',
+  '유니크',
+];
+const spaceOptions = [
+  '원룸',
+  '거실',
+  '침실',
+  '주방',
+  '욕실',
+  '아이방',
+  '드레스룸',
+  '서재&작업실',
+  '베란다',
+  '가구&소품',
+];
+
+export { sizeOptions, stlyeOptions, lifeTypeOptions, spaceOptions };
