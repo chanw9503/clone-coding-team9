adf45bc1201525180ffde7f87e46fe905455315a
ChanwooPark <chanw9503@gmail.com>
2023-03-30 12:02:05 +0900

    [STYLE] : 🎨스타일 기본값 세팅

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,7 +1,7 @@
-import styled, { css } from 'styled-components';
 import React, { useState } from 'react';
-
+import { SelectInput } from './styles';
 function Select({ options, defaultValue, value, onChange, validate }) {
+  if (validate === undefined) validate = true;
   const [seleted, setSeleted] = useState(value || '');
 
   const optionHandlerChange = (e) => {
@@ -26,27 +26,3 @@ function Select({ options, defaultValue, value, onChange, validate }) {
   );
 }
 export default Select;
-
-const SelectInput = styled.select`
-  font-size: 15px;
-  width: 100%;
-  transition: border-color 0.2s, box-shadow 0.2s, background-color 0.2s;
-  display: block;
-  box-sizing: border-box;
-  height: 40px;
-  line-height: 40px;
-  border-radius: 4px;
-
-  background-color: #fff;
-  color: #424242;
-  padding: 0 15px;
-  padding-right: 30px;
-  ${({ validate }) =>
-    validate
-      ? css`
-          border: 1px solid #dbdbdb;
-        `
-      : css`
-          border: 1px solid red;
-        `}
-`;
