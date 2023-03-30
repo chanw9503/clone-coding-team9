c56caa0c9898c2c07ea2750db5ac32c152011f6e
ChanwooPark <chanw9503@gmail.com>
2023-03-30 12:04:44 +0900

    [FEAT] : ⚙️Header Logout add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LogoType from '../LogoType/LogoType';
 import Navigation from '../Navigation';
@@ -14,9 +14,26 @@ import {
   StyledWritingButton,
   StyledFixBlock,
 } from './styles';
+import { getCookie, removeCookie } from '../../auth/Cookie';
 
 function Header() {
   const navigate = useNavigate();
+  const cookies = getCookie('token');
+
+  const [logout, setLogout] = useState(false);
+
+  const logoutHandler = () => {
+    if (window.confirm('로그아웃 하시겠습니까>')) {
+      setLogout(true);
+      removeCookie('token');
+    }
+  };
+
+  const loginHandler = () => {
+    navigate('/login');
+    setLogout(false);
+  };
+
   return (
     <StyledHeader>
       <StyledContainer>
@@ -24,16 +41,26 @@ function Header() {
           <StyledBoardBlock>
             <StyledItemBlock>
               <StyledFlexBlock>
-                <LogoType />
+                <LogoType onClick={() => navigate('/')} />
               </StyledFlexBlock>
               <StyledFlexBlock>
-                <StyledLoginBlock onClick={() => navigate('/login')} borderLight={true}>
-                  로그인
-                </StyledLoginBlock>
+                {cookies ? (
+                  <StyledLoginBlock onClick={logoutHandler} borderLight={true}>
+                    로그아웃
+                  </StyledLoginBlock>
+                ) : (
+                  <StyledLoginBlock onClick={loginHandler} borderLight={true}>
+                    로그인
+                  </StyledLoginBlock>
+                )}
                 <StyledLoginBlock onClick={() => navigate('/signup')}>
                   회원가입
                 </StyledLoginBlock>
-                <StyledWritingButton>
+                <StyledWritingButton
+                  onClick={() => {
+                    navigate('/posts');
+                  }}
+                >
                   <span
                     style={{ fontSize: '12px', lineHeight: 18, verticalAlign: 'middle' }}
                   >
