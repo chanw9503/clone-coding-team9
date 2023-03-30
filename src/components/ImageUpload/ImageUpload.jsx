9e58a29db69c0850dcd0e8c5512e5093a8dee1ba
ChanwooPark <chanw9503@gmail.com>
2023-03-30 12:04:18 +0900

    [RETACTOR] : 🏭 이미지 업로드 방식 병경 createObjectURL -> s3를 이용한 URL 받는 형식으로 변경

--- src/components/ImageUpload/ImageUpload.jsx
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { StSendImage, StText, StyledImage } from './Styles';
+import { StSendImage, StText } from './Styles';
 import SendImageIcon from '../../pages/Posts/SendImageIcon';
-import { useEffect } from 'react';
 import { uploadFile } from '../../utils/upload';
+import { useDispatch } from 'react-redux';
+import { addContents, initBoards } from '../../redux/Modules/boardSlice';
 
 function ImageUpload(props) {
   const saveImage = (file) => {
     if (file) {
-      URL.revokeObjectURL(props.image.preview_URL);
-      const preview_URL = URL.createObjectURL(file);
-
-      props.setImage({
-        image_file: file,
-        preview_URL: preview_URL,
-      });
-
-      const s3Location = uploadFile(file); //URL로 받기
-      /**
-       * 부모에서 내려주는 props 추가
-       * const newLocationList = locationList
-       * setLocationList(newLocationList)
-       */
+      getUrl(file);
     }
+  };
+
+  const dispatch = useDispatch();
+
+  const getUrl = async (file) => {
+    const s3Location = await uploadFile(file); //URL로 받기
+    const newBoards = {
+      img: s3Location,
+    };
+    dispatch(addContents(props.boardsRef.current));
+    dispatch(initBoards(newBoards));
   };
 
   const ChangeHandler = (e) => {
@@ -29,15 +28,6 @@ function ImageUpload(props) {
     const files = e.target.files;
     saveImage(files[0]);
   };
-
-  useEffect(() => {
-    if (props.defaultImage !== '') {
-      props.setImage({ ...props.image, preview_URL: props.defaultImage });
-    }
-    return () => {
-      URL.revokeObjectURL(props.image.preview_URL);
-    };
-  }, []);
 
   return (
     <>
@@ -51,15 +41,11 @@ function ImageUpload(props) {
           required
           onChange={ChangeHandler}
         />
-        {props.image.preview_URL ? (
-          <StyledImage src={props.image.preview_URL} />
-        ) : (
-          <div>
-            <SendImageIcon />
-            <StText>사진올리기</StText>
-            <StText style={{ fontSize: '12px' }}>(*최대 10장까지)</StText>
-          </div>
-        )}
+        <div>
+          <SendImageIcon />
+          <StText>사진올리기</StText>
+          <StText style={{ fontSize: '12px' }}>(*최대 10장까지)</StText>
+        </div>
       </StSendImage>
     </>
   );
